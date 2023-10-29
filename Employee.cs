cea3fa099dbb906e5d380d065a302f4b17e92b86
TIAGO NEVES LEAO COSTA LINO <tiagobrasil18@gmail.com>
2023-10-29 08:41:56 -0300

    lesson 17

---
+++ Employee.cs
@@ -0,0 +1,10 @@
+﻿namespace Demo
+{
+    public class Employee
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string gender { get; set; }
+        public int salary { get; set; }
+    }
+}
---
+++ Scripts/Script.js
@@ -0,0 +1,11 @@
+/// <reference path="angular.min.js" />
+
+var app = angular
+        .module("myModule", [])
+        .controller("myController", function ($scope, $http) {
+
+            $http.get("EmployeeService.asmx/GetAllEmployees")
+                 .then(function (response) {
+                     $scope.employees = response.data;
+                 });
+        });
